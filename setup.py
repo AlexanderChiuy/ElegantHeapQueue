41de2884b7906904f9b24bf664c2bff03a04dff6
Tyler Lowe <tylerlowe@gmail.com>
2022-12-07 20:22:24 -0500

    Add v0.0.2 setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='HeapQueue',
-    version='0.0.1',
+    version='0.0.2',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/AlexanderChiuy/HeapQueue',
