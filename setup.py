5ae541a1cd6b42c6f57b6028fbcf2227b77829e7
tylowe-labs <59340807+tylowe-labs@users.noreply.github.com>
2022-12-07 22:18:17 -0500

    Update setup.py for initial release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='elegant-heap-queue',
-    version='0.0.1',
+    version='1.0.0',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/AlexanderChiuy/ElegantHeapQueue',
