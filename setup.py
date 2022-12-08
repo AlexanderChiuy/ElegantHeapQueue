959856aaea81672c56c774ec1bb52e091e93dbcb
tylowe-labs <59340807+tylowe-labs@users.noreply.github.com>
2022-12-07 19:46:52 -0500

    Update setup.py
    Update version

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='HeapQueue',
+    version='0.0.1',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/AlexanderChiuy/HeapQueue',
